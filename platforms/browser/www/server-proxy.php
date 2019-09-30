bd6c0bf6dca1189459350739c4d03dd8d78c8f63
Iván David Vázqeuez Rivera <ivan.vazquez@jalisco.gob.mx>
2019-09-30 17:44:41 -0500

    init

---
+++ platforms/browser/www/server-proxy.php
@@ -0,0 +1,4 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+echo file_get_contents('http://201.131.6.16/information/find_from_portal_request.json?auth_token=z9KdkBgWansrozTK9wcy&plate_number=JLZ2515&serial_number=3N1EB31SXY224305');
+?>
